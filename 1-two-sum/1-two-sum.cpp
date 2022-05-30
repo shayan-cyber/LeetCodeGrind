90f86847b8884d8126510402f796859869e34240
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-30 11:29:57 +0530

    Time: 27 ms (45.46%), Space: 11 MB (24.14%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,22 +3,62 @@ class Solution
     public:
         vector<int> twoSum(vector<int> &nums, int target)
         {
-
-            vector<int> v;
-            unordered_map <int,int> mp;
+    
+            vector<int> ans;
+            
+            
+//             hashmap approach
+            
+//             unordered_map <int,int> mp;
+            
+            
+//             for(int i=0; i<nums.size(); i++){
+//                 if(mp.find(target - nums[i]) != mp.end()){
+//                    ans.push_back(i);
+//                    ans.push_back(mp[target-nums[i]]);
+//                     break;
+//                 }else{
+//                     mp[nums[i]] = i;
+//                 }
+//             }
+            
+            
+            
+//             two pointer approach
+            
+            
+            vector <pair <int,int>> v;
             
             
             for(int i=0; i<nums.size(); i++){
-                if(mp.find(target - nums[i]) != mp.end()){
-                    v.push_back(i);
-                    v.push_back(mp[target-nums[i]]);
+                v.push_back(make_pair(nums[i], i));
+            }
+            
+            sort(v.begin(), v.end());
+            
+            
+            int s= 0;
+            int e = nums.size()-1;
+            
+            while(s<e){
+                if(v[s].first + v[e].first == target){
+                    ans.push_back(v[s].second);
+                    ans.push_back(v[e].second);
                     break;
+                        
+                }else if(v[s].first + v[e].first > target){
+                    e--;
                 }else{
-                    mp[nums[i]] = i;
+                    s++;
                 }
             }
             
-            return v;
+            
+            
+            
+            
+            
+            return ans;
             
             
         }
