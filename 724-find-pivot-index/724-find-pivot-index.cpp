ad1da871562ac086896849ba761a7b14d01703a2
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-30 20:09:25 +0530

    Time: 16 ms (98.51%), Space: 31.1 MB (53.92%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,27 +2,50 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         
-        int sum =0;
+//         int sum =0;
+//         int n = nums.size();
+//         int prevSum =0;
+        
+//         for(int i=0; i<n; i++){
+//             sum += nums[i];
+//         }
+        
+        
+//         for(int i=0; i<n; i++){
+//             if((sum - nums[i]) % 2 == 0 && (sum - nums[i])/2 == prevSum){
+                
+//                 return i;
+                
+//             }
+//             prevSum += nums[i];
+            
+            
+//         }
+        
+//         return -1;
+        
+        
+        
         int n = nums.size();
-        int prevSum =0;
+        int left[n];
+        int right[n];
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
         
-        for(int i=0; i<n; i++){
-            sum += nums[i];
+        for(int i=1; i<n; i++){
+            
+            left[i] = nums[i] + left[i-1];
+            right[n-i-1] = nums[n-i-1] + right[n-i];
         }
         
-        
         for(int i=0; i<n; i++){
-            if((sum - nums[i]) % 2 == 0 && (sum - nums[i])/2 == prevSum){
-                
+            if(left[i] == right[i])
                 return i;
-                
-            }
-            prevSum += nums[i];
-            
-            
         }
+       
         
         return -1;
+        
         
     }
 };
