104e5772c1542c8e712fcc7af14aaee0534abcbd
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-30 15:03:14 +0530

    Time: 35 ms (42.99%), Space: 20.9 MB (5.32%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,24 +4,56 @@ public:
         
         int n= nums.size();
         
-        int count = 0;
         
-        for(int i=0; i<n; i++){
-            if(nums[i] == 0)
-                count ++;
-        }
+//         brute force
+//         int count = 0;
+        
+//         for(int i=0; i<n; i++){
+//             if(nums[i] == 0)
+//                 count ++;
+//         }
         
         
-        while(count >0){
-            for(int i=0; i< n-1; i++){
-                if(nums[i] == 0){
-                    swap(nums[i+1], nums[i]);
-                }
+//         while(count >0){
+//             for(int i=0; i< n-1; i++){
+//                 if(nums[i] == 0){
+//                     swap(nums[i+1], nums[i]);
+//                 }
+//             }
+                
+                
+                
+//             count --;
+//         }
+        
+        
+        
+//         optimized
+        
+        vector <pair <int,int>> v;
+        int prevCount =0;
+        
+        
+        for(int i =0; i<n; i++){
+            if(nums[i] != 0){
+                v.push_back(make_pair(nums[i], i-prevCount));
+                // prevCount =0;
+            }else{
+                prevCount ++;
             }
-                
-                
-                
-            count --;
         }
+        
+        
+        for(int i=n-1; i>=n-prevCount; i--){
+            
+            nums[i]=0;
+            
+        }
+        
+        for(int i=0; i<v.size(); i++){
+            nums[v[i].second] = v[i].first;
+        }
+        
+        
     }
 };
