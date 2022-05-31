51a9c5c8f314a9981c895f1db6f22b196607073e
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-31 11:25:16 +0530

    Time: 76 ms (9.43%), Space: 26 MB (55.83%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -3,38 +3,72 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         
         int n= nums.size();
-        int count =-1;
-        for(int i=0; i<n; i++){
+        
+//         merge sorted arrays
+//         int count =-1;
+//         for(int i=0; i<n; i++){
             
-            if(nums[i]<0)
-                count ++;
-            nums[i] = nums[i]*nums[i];
+//             if(nums[i]<0)
+//                 count ++;
+//             nums[i] = nums[i]*nums[i];
             
-        }
-        // cout<<count;
-        vector<int> v;
-        int i=count;
-        int j = count +1;
-        while(i>=0 && j<n){
-                if(nums[j]<nums[i]){
-                    v.push_back(nums[j]);
-                    j++;
+//         }
+//         // cout<<count;
+//         vector<int> v;
+//         int i=count;
+//         int j = count +1;
+//         while(i>=0 && j<n){
+//                 if(nums[j]<nums[i]){
+//                     v.push_back(nums[j]);
+//                     j++;
                     
-                }else{
-                    v.push_back(nums[i]);
-                    i--;
-                }
+//                 }else{
+//                     v.push_back(nums[i]);
+//                     i--;
+//                 }
+//         }
+        
+//         while(j<n){
+//             v.push_back(nums[j]);
+//             j++;
+//         }
+        
+//         while(i>=0){
+//             v.push_back(nums[i]);
+//             i--;
+//         }
+        
+//         return v;
+        
+        
+        
+//         two pointer
+        
+        
+        vector <int> v(n);
+        
+        int i=0;
+        int j=n-1;
+        
+        int pos= n-1;
+        
+        while(i<=j){
+            
+            if(abs(nums[i]) > abs(nums[j])){
+                v[pos] = nums[i]*nums[i];
+                pos --;
+                i++;
+                    
+            }else{
+                v[pos] = nums[j]*nums[j];
+                pos --;
+                j--;
+                
+            }
+            
+            
         }
         
-        while(j<n){
-            v.push_back(nums[j]);
-            j++;
-        }
-        
-        while(i>=0){
-            v.push_back(nums[i]);
-            i--;
-        }
         
         return v;
         
