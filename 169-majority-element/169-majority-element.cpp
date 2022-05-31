4c94934aa317cd8fbfdb2d69a1fc363401a57f6a
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-31 09:21:17 +0530

    Time: 41 ms (10.12%), Space: 19.5 MB (92.57%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,26 +3,59 @@ public:
     int majorityElement(vector<int>& nums) {
         
         int n = nums.size();
-        unordered_map <int,int> mp ;
         
-        for(int i=0; i<n; i++){
+//         using map
+//         unordered_map <int,int> mp ;
+        
+//         for(int i=0; i<n; i++){
             
-            if(mp.find(nums[i]) != mp.end()){
-                mp[nums[i]] ++;
+//             if(mp.find(nums[i]) != mp.end()){
+//                 mp[nums[i]] ++;
+//             }else{
+//                 mp[nums[i]] =1;
+//             }
+//         }
+        
+        
+        
+//         for(auto i:mp){
+//             if(i.second>n/2){
+//                 return i.first;
+//             }
+//         }
+        
+//         return -1;
+        
+        
+        
+//         Boyer–Moore majority vote algorithm 
+        
+        
+        int c=nums[0];
+        int v = 1;
+        
+        
+        for(int i=1; i<n; i++){
+            
+            
+            if(nums[i] == c){
+                v++;
+                continue;
             }else{
-                mp[nums[i]] =1;
+                v--;
             }
+            
+            if(v==0){
+                c = nums[i];
+                v= 1;
+            }
+            
         }
         
+        return c;
         
         
-        for(auto i:mp){
-            if(i.second>n/2){
-                return i.first;
-            }
-        }
         
-        return -1;
         
     }
 };
