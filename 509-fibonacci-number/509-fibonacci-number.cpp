a70b96551ac4ada90989ed3f6cb172c85c0c920a
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-05-31 10:15:57 +0530

    Time: 3 ms (59.27%), Space: 5.8 MB (96.54%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
+//     using normal recursion
+//     int Solve(int n){
+//         if(n == 0)
+//             return 0;
+//         if(n==1)
+//             return 1;
+//         return Solve(n-1) + Solve(n-2);
+//     }
     
+    
+//     using 1D dp 
+    
+    int dp[31]={0};
     int Solve(int n){
-        if(n == 0)
-            return 0;
-        if(n==1)
-            return 1;
-        return Solve(n-1) + Solve(n-2);
+        if(n<=1){
+            return n;
+        }
+        
+        if(dp[n])
+            return dp[n];
+        
+        return dp[n] = Solve(n-1) + Solve(n-2);
     }
+    
+    
     
     int fib(int n) {
         
