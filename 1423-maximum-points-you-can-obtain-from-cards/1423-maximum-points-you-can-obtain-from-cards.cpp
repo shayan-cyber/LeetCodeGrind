6a8142cd3deddc43869002e42e5d5c39ac76f473
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-04 11:22:12 +0530

    Time: 74 ms (68.89%), Space: 42.4 MB (49.49%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,44 +1,45 @@
-class Solution {
-public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        
-        int n= cardPoints.size();
-        
-        int sum =0;
-        
-        for(int i=0; i<n; i++){
-            sum+=cardPoints[i];
-        }
-        
-       
-        
-        cout<<sum;
-        
-        int sumK =0;
-        for(int i=0; i<n-k; i++){
-            sumK+=cardPoints[i];
-        }
-        int mn = INT_MAX;
-        // cout<<mn<<" ";
-        int i=0;
-        int j = n-k;
-        int temp= sumK;
-        
-        while(j<=n){
-           
-        
-          
-            mn = min(mn,temp );
-              if(j == n)break;
+class Solution
+{
+    public:
+        int maxScore(vector<int> &cardPoints, int k)
+        {
+
+            int n = cardPoints.size();
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += cardPoints[i];
+            }
+
+      
+
+            int sumK = 0;
+            for (int i = 0; i < n - k; i++)
+            {
+                sumK += cardPoints[i];
+            }
             
-            temp = temp - cardPoints[i] + cardPoints[j];
-        
-            i++;
-            j++;
+            
+            int mn = INT_MAX;
+            int i = 0;
+            int j = n - k;
+            int temp = sumK;
+
+            while (j <= n)
+            {
+
+                mn = min(mn, temp);
+                if (j == n) break;
+
+                temp = temp - cardPoints[i] + cardPoints[j];
+
+                i++;
+                j++;
+            }
+           
+
+            return sum - mn;
         }
-       cout<<" "<<temp<<j;
-        
-        return sum - mn;
-        
-    }
 };
