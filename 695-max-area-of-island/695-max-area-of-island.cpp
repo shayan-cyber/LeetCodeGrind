3466cefb376da0ce926b9f93545e81ff61066614
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-04 17:43:34 +0530

    Time: 23 ms (71.07%), Space: 23.2 MB (89.57%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     
-    int Solve(vector<vector<int>> grid, int i, int j, set <pair<int,int>> &s){
+    int Solve(vector<vector<int>> &grid, int i, int j){
         int m = grid.size();
         int n = grid[0].size(); 
-        if(i<0|| j<0 || i>=m || j>=n || grid[i][j] ==0 || s.find(make_pair(i,j)) != s.end()){
+        if(i<0|| j<0 || i>=m || j>=n || grid[i][j] == 0 ){
             return 0;
         }
+        grid[i][j] = 0;
+        // v[i][j]==1;
         
-        if(grid[i][j])
-            s.insert(make_pair(i,j));
-        return 1 + Solve(grid, i+1, j,s) + Solve(grid, i-1, j,s) + Solve(grid, i, j+1,s) + Solve(grid, i, j-1,s);
+
+        return 1 + Solve(grid, i+1, j) + Solve(grid, i-1, j) + Solve(grid, i, j+1) + Solve(grid, i, j-1);
         
     }
     
@@ -19,14 +20,18 @@ public:
         int maxA = 0;
         int m = grid.size();
         int n = grid[0].size();
-        set <pair<int,int>> s;
+        // set <pair<int,int>> s;
+        //instead of set we'll use visited matrix to decrease time complexity to find cause find of set takes O(logn) time
+//         we can also optimize more cause visted array gives TLE ..we can modify grid[i][j] =0 if visted that way we'll not visit (go to solve function call) again
+        // vector<vector<int>>v(m, vector<int>(n,0));
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1){
-                    if(s.find(make_pair(i,j)) == s.end()){
+                    // if(v[i][j] ==0){
                             
-                    maxA = max(maxA, Solve(grid, i,j,s));
-                    }
+                    maxA = max(maxA, Solve(grid, i,j));
+                    // }
                     // cout<<Solve(grid,i,j,s)<<" ";
                 
                     
