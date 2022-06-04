1343ff736e591b96875edd531e444bb0ff835d40
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-04 20:50:35 +0530

    Time: 20 ms (72.01%), Space: 13.3 MB (29.77%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -3,47 +3,81 @@ public:
     int findPairs(vector<int>& nums, int k) {
         
         int n = nums.size();
-        set <int> s;
+//         first approach
+//         set <int> s;
    
-        vector<int> temp = nums;
-        set <int> duplicates;
-        set <pair<int,int>> check;
+//         vector<int> temp = nums;
+//         set <int> duplicates;
+//         set <pair<int,int>> check;
         
         
         
-        for(int i=0; i<n; i++){
-            if(s.find(nums[i]) != s.end()){
-                duplicates.insert(nums[i]);
-            }
-            s.insert(nums[i]);
-        }
+//         for(int i=0; i<n; i++){
+//             if(s.find(nums[i]) != s.end()){
+//                 duplicates.insert(nums[i]);
+//             }
+//             s.insert(nums[i]);
+//         }
         
-        for(int i=0;i<n;i++){
+//         for(int i=0;i<n;i++){
             
-            nums[i] = nums[i]-k;
-        }
+//             nums[i] = nums[i]-k;
+//         }
+        
+//         int count =0;
+//         for(int i=0; i<n; i++){
+            
+//             if(s.find(nums[i]) != s.end()){
+//                 pair<int,int>p; 
+//                 if(nums[i] > temp[i]){
+//                     p = make_pair(temp[i], nums[i]);
+//                 }else{
+//                     p = make_pair(nums[i],temp[i]);
+//                 }
+                
+//                 if(check.find(p) == check.end()){
+//                     check.insert(p);
+//                     count++;
+//                 }
+                
+               
+//             }
+//         }
+//         if(k==0)
+//             return duplicates.size();
+        
+        
+//         easier approach;
+        
         
         int count =0;
-        for(int i=0; i<n; i++){
-            
-            if(s.find(nums[i]) != s.end()){
-                pair<int,int>p; 
-                if(nums[i] > temp[i]){
-                    p = make_pair(temp[i], nums[i]);
-                }else{
-                    p = make_pair(nums[i],temp[i]);
-                }
-                
-                if(check.find(p) == check.end()){
-                    check.insert(p);
+        unordered_map <int,int> mp;
+        
+        
+        for(int i=0; i<n ; i++){
+            if(mp.find(nums[i]) != mp.end())
+                mp[nums[i]]++;
+            else
+            mp[nums[i]] =1;
+        }
+        
+        if(k==0){
+            for(auto a:mp){
+                // cout<<a.first<<" "<<a.second<<" ";
+                if(a.second>1){
+                    
                     count++;
                 }
-                
-                // count++;
+            }
+        }else{
+            for(auto it :mp){
+                if(mp.find(it.first+k) != mp.end())
+                    count++;
             }
         }
-        if(k==0)
-            return duplicates.size();
+        
+        
+        
         
         
       return count; 
