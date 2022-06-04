3b2f05eba351aa11e1e3bfd012e52d9cf28a2fa1
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-04 13:51:19 +0530

    Time: 92 ms (57.68%), Space: 32.3 MB (37.95%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -7,15 +7,27 @@ class Solution
 
             int max_h = INT_MIN;
             int max_w = INT_MIN;
+            
+            //sort the arrays because largets indexed cuts will be furthers cut down by smaller indexed cut and will make smaller cut
             sort(horizontalCuts.begin(), horizontalCuts.end());
             sort(verticalCuts.begin(), verticalCuts.end());
+            
+            
+            //largest height of cut initially will be horizontalCuts[0] or h-horizontalCuts[n-1]
             max_h = max(horizontalCuts[0], h - horizontalCuts.back());
+            
+            
+            // largest width of cut initially will be verticalCuts[0] or w - verticalCuts[n-1]
             max_w = max(verticalCuts[0], w - verticalCuts.back());
-
+            
+            
+            
             for (int i = 1; i < horizontalCuts.size(); i++)
             {
                 max_h = max(max_h, horizontalCuts[i] - horizontalCuts[i - 1]);
             }
+            
+            
             for (int i = 1; i < verticalCuts.size(); i++)
             {
                 max_w = max(max_w, verticalCuts[i] - verticalCuts[i - 1]);
