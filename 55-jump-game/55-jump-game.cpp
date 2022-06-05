23e0d586e93c4a1b3dbbb6dcf6148a056432b884
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-05 12:00:09 +0530

    Time: 129 ms (28.48%), Space: 48.3 MB (77.95%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+            //first approach
+//         int i=0; 
+//         int currReach = 0;
+//         int currMax = 0;
         
-        int i=0; 
-        int currReach = 0;
-        int currMax = 0;
+//         int n = nums.size();
+        
+//         while(i<n-1){
+            
+//             currMax = max(currMax, nums[i] +i) ;
+//             if(currReach==i){
+//                 currReach = currMax;
+//             }    
+//             i++;
+            
+            
+//         }
+//         // cout<<currReach<<" ";
+//         if(currReach >=n-1){
+//             return true;
+//         }else{
+            
+//             return false;
+//         }
+        
+        
+        //second approach
+        
+        
         
         int n = nums.size();
+        int maxJumpenergy = 0;
         
-        while(i<n-1){
+        for(int i=0; i<n; i++){
+            if(maxJumpenergy<0)
+                return false;
             
-            currMax = max(currMax, nums[i] +i) ;
-            if(currReach==i){
-                currReach = currMax;
-            }    
-            i++;
-            
-            
+            maxJumpenergy = max(maxJumpenergy,nums[i])-1;
         }
-        cout<<currReach<<" ";
-        if(currReach >=n-1){
-            return true;
-        }else{
-            
-            return false;
-        }
+        
+        
+        return true;
+        
         
         
         
