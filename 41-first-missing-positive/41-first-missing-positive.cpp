4a0ef253810c59fca96e1002870fc0a40f99b08d
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-06 18:43:06 +0530

    Time: 232 ms (46.79%), Space: 82.9 MB (78.14%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -5,20 +5,52 @@ public:
         
         
         //first approach (brute force not ideal cause it'll take nlogn time in worst case)
-        int n =nums.size();
-        sort(nums.begin(), nums.end());
-        int curr =1;
+//         int n =nums.size();
+//         sort(nums.begin(), nums.end());
+//         int curr =1;
+        
+//         for(int i=0; i<n; i++){
+//             if(curr > nums[i])
+//                 continue;
+//             if(curr < nums[i])
+//                 return curr;
+//             else
+//                 curr++;
+//         }
+        
+//         return curr;
+        
+        
+        
+        
+//         second approach from YT help
+        
+        int n = nums.size();
+            
         
         for(int i=0; i<n; i++){
-            if(curr > nums[i])
-                continue;
-            if(curr < nums[i])
-                return curr;
-            else
-                curr++;
+            
+            if(nums[i]<=0)
+                nums[i] = n+1;
         }
         
-        return curr;
+        
+        for(int i=0; i<n; i++){
+            if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0){
+                nums[abs(nums[i])-1] = nums[abs(nums[i])-1]*(-1);
+            }
+            
+            
+        }
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]>0)
+                return i+1;
+        }
+        
+        
+        return n+1;
+        
         
         
     }
