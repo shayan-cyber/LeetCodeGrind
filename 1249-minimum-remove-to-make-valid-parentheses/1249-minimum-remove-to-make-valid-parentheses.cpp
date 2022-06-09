c380bf87ccb91bc7d4f417cb2a6ab1ea775a7fb6
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-09 12:51:20 +0530

    Time: 19 ms (92.02%), Space: 11.6 MB (52.69%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,9 +2,61 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int n = s.length();
+        //first approach using extra space vector for invalid parenthesis.
+        
+//         stack <int> st;
+//         vector <int> q;
+        
+//         for(int i=0; i<n; i++){
+//             if(s[i] == ')'){
+                
+                
+//                 if(!st.empty() ){
+//                     st.pop();
+//                 }else{
+     
+//                     q.push_back(i);
+//                 }
+                
+//             }else if(s[i] == '('){
+//                 st.push(i);
+//             }
+//         }
+        
+        
+//         while(st.empty() == false){
+//             int temp = st.top();
+//             q.push_back(temp);
+//             st.pop();
+            
+//         }
+
+//         sort(q.begin(), q.end());
+//         queue <int> qq;
+//         for(int i=0; i<q.size() ;i++){
+
+//             qq.push(q[i]);
+//         }
+//         string ans="";
+        
+//         for(int i=0; i<n; i++){
+//             if(qq.front() == i){
+              
+//                 qq.pop();
+//                 continue;
+//             }
+//             ans.push_back(s[i]);
+            
+//         }
+        
+        
+//         return ans;
+        
+        
+//         second approach using invalid char '#' for invalid parenthesis.
         
         stack <int> st;
-        vector <int> q;
+      
         
         for(int i=0; i<n; i++){
             if(s[i] == ')'){
@@ -13,8 +65,8 @@ public:
                 if(!st.empty() ){
                     st.pop();
                 }else{
-                    // cout<<"here"<<i<<" "<<endl;
-                    q.push_back(i);
+     
+                    s[i]='#';
                 }
                 
             }else if(s[i] == '('){
@@ -25,31 +77,22 @@ public:
         
         while(st.empty() == false){
             int temp = st.top();
-            q.push_back(temp);
+            s[temp] = '#';
             st.pop();
             
         }
-        // cout<<q.size()<<endl;
-        sort(q.begin(), q.end());
-        queue <int> qq;
-        for(int i=0; i<q.size() ;i++){
-            // cout<<i<<" ";
-            qq.push(q[i]);
-        }
+
+        
         string ans="";
         
         for(int i=0; i<n; i++){
-            if(qq.front() == i){
-                // cout<< s[qq.front()] <<qq.front()<<" ";
-                qq.pop();
+            if(s[i] == '#')
                 continue;
-            }
             ans.push_back(s[i]);
             
         }
         
-        // cout<<endl;
-        // cout<<q.size();
+        
         return ans;
         
          
