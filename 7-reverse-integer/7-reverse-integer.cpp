8a2951601f5003081f79e695aedd97bf331d0c26
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-12 12:18:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.83%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        long long int res =0;
+        long long int rem = 0;
+        int n = x;
+        
+        long long int temp = pow(2,31);
+        
+        while(n != 0){
+            rem = n%10;
+            n = n/10;
+            res = res *10 + rem;
+            
+        }
+        
+        
+        if(res < -temp || res> temp-1)
+            return 0;
+        return res;
+        
+    
+        
+        
+    }
+};
