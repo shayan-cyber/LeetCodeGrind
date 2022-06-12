0ad5f4d83c1c7bfd2c0403d7c26d2deff4ea2449
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-12 15:21:05 +0530

    Time: 5 ms (28.63%), Space: 5.8 MB (94.38%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,35 +1,67 @@
-class Solution {
-public:
-    bool isHappy(int n) {
-        
-        
-        if(n==1){
+class Solution
+{
+    public:
+        int getSqrSum(int n)
+        {
+
+            int temp = n;
+            int temp2 = 0;
+            while (temp != 0)
+            {
+                temp2 = temp2 + (temp % 10) *(temp % 10);
+                temp = temp / 10;
+            }
+            
+            return temp2;
+        }
+
+    bool isHappy(int n)
+    {
+
+        if (n == 1)
+        {
             return true;
         }
+       	//         first approach using set to detect loop.
+       	//         set<int> mp;
+
+       	//         int x= n;
+       	//         while(true){
+
+       	//             if(x==1){
+       	//                 return true;
+       	//             }
+       	//             if(mp.find(x) != mp.end()){
+       	//                 return false;
+       	//             }
+       	//             mp.insert(x);
+       	//             int temp =x;
+       	//             int temp2 =0;
+       	//             while(temp!= 0){
+       	//                 temp2 = temp2 + (temp%10)*(temp%10);
+       	//                 temp = temp/10;
+
+       	//             }
+
+       	//             x = temp2;
+
+       	//         }
+
         
-        set <int> mp;
         
-        int x= n;
-        while(true){
-            
-            if(x==1){
-                return true;
-            }
-            if(mp.find(x) != mp.end()){
-                return false;
-            }
-            mp.insert(x);
-            int temp =x;
-            int temp2 =0;
-            while(temp!= 0){
-                temp2 = temp2 + (temp%10)*(temp%10);
-                temp = temp/10;
-                
-            }
-            
-            x = temp2;
-            
-        }
+       	//         second approach to detect loop using tortoise and hare method(floyd's cycle detection)
         
+        int slow = n;
+        int fast = n;
+        
+        do{
+            slow = getSqrSum(slow);
+            fast = getSqrSum(getSqrSum(fast));
+            
+            
+        }while(slow!=fast);
+        
+        return slow== 1;
+
     }
 };
