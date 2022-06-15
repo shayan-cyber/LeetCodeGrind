4a49faeceebbda2f7c32ac8ddd2dc687c19e57e0
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-06-15 21:39:32 +0530

    Time: 593 ms (55.28%), Space: 89.8 MB (46.13%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    static bool myComp(pair<int,int> a,pair<int,int> b)
-    {
-        return (a.second<b.second);
+    
+    bool static mycomp (vector<int> &a, vector<int> &b){
+        if(a[1] == b[1])
+            return a[0] > b[0];
+        return a[1]<b[1];
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int ans=0;
-        vector<pair<int,int>> arr;
-        for(auto it : intervals)
-        {
-            arr.push_back({it[0],it[1]});
-        }
-        sort(arr.begin(),arr.end(),myComp);
-        int prev=0;
-        for(int i=1;i<arr.size();i++)
-        {
-            if(arr[prev].second>arr[i].first){
-                ans++;
-            }
-            else{
-                prev=i;
-            }
+        
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), mycomp);
+        int last = intervals[0][1];
+        int res =0;
+        for(int i=1;i<n; i++){
+            if(intervals[i][0]< last){
+                res++;
+                
+            }else
+                last = intervals[i][1];
+            
             
         }
-        return ans;
+        
+        return res;
+        
+        
     }
 };
