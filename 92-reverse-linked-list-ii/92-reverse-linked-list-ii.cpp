309f6f2776ac03b78b3533031ebbf05a1d14b4f0
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-03 11:20:22 +0530

    Time: 4 ms (51.05%), Space: 7.6 MB (17.59%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -11,67 +11,113 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
-        int count =0;
-        
-        
-        ListNode* curr =head;
-        
-        stack <int> st;
+//    using stack     
+//         int count =0;
         
         
-        while(curr){
+//         ListNode* curr =head;
+        
+//         stack <int> st;
+        
+        
+//         while(curr){
             
-            count ++;
-            if(count >= left && count<= right){
-                st.push(curr-> val);
-            }
+//             count ++;
+//             if(count >= left && count<= right){
+//                 st.push(curr-> val);
+//             }
             
-            curr = curr -> next;
+//             curr = curr -> next;
             
             
-        }
+//         }
         
         
         
-        curr = head;
-        ListNode* myhead = NULL;
-        ListNode* mycurr = NULL;
+//         curr = head;
+//         ListNode* myhead = NULL;
+//         ListNode* mycurr = NULL;
         
         
-        count =0;
-        while(curr){
-            count ++;
-            if(count >= left && count<= right){
-                if(myhead == NULL){
-                    myhead = new ListNode(st.top());
-                    st.pop();
-                    mycurr = myhead;
-                }else{
-                    mycurr -> next = new ListNode(st.top());
-                    st.pop();
-                    mycurr = mycurr -> next;
-                }
+//         count =0;
+//         while(curr){
+//             count ++;
+//             if(count >= left && count<= right){
+//                 if(myhead == NULL){
+//                     myhead = new ListNode(st.top());
+//                     st.pop();
+//                     mycurr = myhead;
+//                 }else{
+//                     mycurr -> next = new ListNode(st.top());
+//                     st.pop();
+//                     mycurr = mycurr -> next;
+//                 }
             
                 
-            }else{
-                if(myhead == NULL){
-                    myhead = new ListNode(curr->val);
-                    mycurr = myhead;
-                }else{
-                    mycurr -> next = new ListNode(curr -> val);
-                    mycurr = mycurr -> next;
+//             }else{
+//                 if(myhead == NULL){
+//                     myhead = new ListNode(curr->val);
+//                     mycurr = myhead;
+//                 }else{
+//                     mycurr -> next = new ListNode(curr -> val);
+//                     mycurr = mycurr -> next;
                     
-                }
+//                 }
                 
-            }
+//             }
             
-            curr = curr -> next;
+//             curr = curr -> next;
+            
+//         }
+        
+        
+//         return myhead;
+        
+        
+        
+        
+        
+//         using stack without making another list
+        
+        
+        ListNode* leftnode = head;
+        
+        for(int i=1; i<left; i++){
+            leftnode = leftnode -> next;
+            
+        }
+        stack<int> st;
+        
+        ListNode* node = leftnode;
+        for(int i=left; i<=right; i++){
+            
+            st.push(node-> val);
+            node = node -> next;
+            
+        }
+        
+        for(int i=left; i<=right; i++){
+            
+            leftnode -> val = st.top();
+            st.pop();
+            leftnode = leftnode -> next;
             
         }
         
         
-        return myhead;
+        return head;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
