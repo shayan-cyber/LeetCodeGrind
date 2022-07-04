be1db521ed22e028084dd0f473173cd1e578040f
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-04 10:54:07 +0530

    Time: 82 ms (25.24%), Space: 19.2 MB (5.96%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cpp
@@ -0,0 +1,90 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+ */
+class Solution
+{
+    public:
+        void reorderList(ListNode *head)
+        {
+
+            queue<ListNode*> q;
+            stack<ListNode*> s;
+
+            int n = 0;
+
+            ListNode *curr = head;
+
+            while (curr)
+            {
+
+                q.push(curr);
+                s.push(curr);
+
+                n++;
+                curr = curr->next;
+            }
+
+           	// cout<<n<<q.size()<<s.size();
+
+            if (n % 2 == 0)
+            {
+
+                for (int i = 1; i <= n / 2; i++)
+                {
+
+                    ListNode *t1 = q.front();
+                    q.pop();
+
+                    ListNode *t2 = s.top();
+                    s.pop();
+
+                    t1->next = t2;
+
+                    cout << i << " ";
+                    if (i < n / 2)
+                    {
+
+                        t2->next = q.front();
+                        cout << "here" << " ";
+                    }
+                    else
+                    {
+                        t2->next = NULL;
+                    }
+
+                   	// if(i<n/2){
+                   	//     ListNode* nxt = q.front();
+                   	//     t2-> next = nxt;
+                   	// }
+                }
+            }
+            else
+            {
+
+                for (int i = 1; i <= n / 2; i++)
+                {
+
+                    ListNode *t1 = q.front();
+                    q.pop();
+
+                    ListNode *t2 = s.top();
+                    s.pop();
+
+                    t1->next = t2;
+
+                    cout << i << " ";
+
+                    t2->next = q.front();
+                    cout << "here" << " ";
+                }
+                s.top()->next = NULL;
+            }
+        }
+};
