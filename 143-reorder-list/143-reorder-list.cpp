b00f6a84e7e9df10180d723cc8913b0623a6a957
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-04 11:10:27 +0530

    Time: 81 ms (26.10%), Space: 18.5 MB (34.28%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -13,78 +13,129 @@ class Solution
     public:
         void reorderList(ListNode *head)
         {
+            
+            
+//              with queue (naive)
+//             queue<ListNode*> q;
+//             stack<ListNode*> s;
 
-            queue<ListNode*> q;
-            stack<ListNode*> s;
+//             int n = 0;
 
-            int n = 0;
+//             ListNode *curr = head;
 
-            ListNode *curr = head;
+//             while (curr)
+//             {
 
-            while (curr)
-            {
+//                 q.push(curr);
+//                 s.push(curr);
 
-                q.push(curr);
+//                 n++;
+//                 curr = curr->next;
+//             }
+
+//            	// cout<<n<<q.size()<<s.size();
+
+//             if (n % 2 == 0)
+//             {
+
+//                 for (int i = 1; i <= n / 2; i++)
+//                 {
+
+//                     ListNode *t1 = q.front();
+//                     q.pop();
+
+//                     ListNode *t2 = s.top();
+//                     s.pop();
+
+//                     t1->next = t2;
+
+//                     cout << i << " ";
+//                     if (i < n / 2)
+//                     {
+
+//                         t2->next = q.front();
+//                         cout << "here" << " ";
+//                     }
+//                     else
+//                     {
+//                         t2->next = NULL;
+//                     }
+
+//                    	// if(i<n/2){
+//                    	//     ListNode* nxt = q.front();
+//                    	//     t2-> next = nxt;
+//                    	// }
+//                 }
+//             }
+//             else
+//             {
+
+//                 for (int i = 1; i <= n / 2; i++)
+//                 {
+
+//                     ListNode *t1 = q.front();
+//                     q.pop();
+
+//                     ListNode *t2 = s.top();
+//                     s.pop();
+
+//                     t1->next = t2;
+
+//                     cout << i << " ";
+
+//                     t2->next = q.front();
+//                     cout << "here" << " ";
+//                 }
+//                 s.top()->next = NULL;
+//             }
+            
+            
+            
+            
+//             without queue
+            
+            stack <ListNode*> s;
+            
+            
+            int n =0;
+            
+            ListNode* curr = head;
+            
+            
+            while(curr){
+                
                 s.push(curr);
-
                 n++;
-                curr = curr->next;
+                curr= curr -> next;
             }
-
-           	// cout<<n<<q.size()<<s.size();
-
-            if (n % 2 == 0)
-            {
-
-                for (int i = 1; i <= n / 2; i++)
-                {
-
-                    ListNode *t1 = q.front();
-                    q.pop();
-
-                    ListNode *t2 = s.top();
-                    s.pop();
-
-                    t1->next = t2;
-
-                    cout << i << " ";
-                    if (i < n / 2)
-                    {
-
-                        t2->next = q.front();
-                        cout << "here" << " ";
-                    }
-                    else
-                    {
-                        t2->next = NULL;
-                    }
-
-                   	// if(i<n/2){
-                   	//     ListNode* nxt = q.front();
-                   	//     t2-> next = nxt;
-                   	// }
-                }
+            
+            
+            curr =  head;
+            
+            for(int i=1; i<=n/2; i++){
+                
+                ListNode* el = s.top();
+                s.pop();
+                
+                el -> next = curr ->next;
+                curr -> next = el;
+                curr = curr -> next -> next;
+                
+                
             }
-            else
-            {
-
-                for (int i = 1; i <= n / 2; i++)
-                {
-
-                    ListNode *t1 = q.front();
-                    q.pop();
-
-                    ListNode *t2 = s.top();
-                    s.pop();
-
-                    t1->next = t2;
-
-                    cout << i << " ";
-
-                    t2->next = q.front();
-                    cout << "here" << " ";
-                }
-                s.top()->next = NULL;
-            }
+            
+            curr -> next = NULL;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         }
 };
