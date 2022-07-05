63220e04fc7c7668aa0106912ba38ce359a5d559
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-05 17:25:48 +0530

    Time: 20 ms (5.71%), Space: 10.4 MB (32.34%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,59 +11,128 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+//         with queue(naive)
         
-        queue <ListNode*> q1;
-        queue <ListNode*> q2;
+//         queue <ListNode*> q1;
+//         queue <ListNode*> q2;
         
-        ListNode* curr =head;
+//         ListNode* curr =head;
+//         if(head == NULL)
+//             return NULL;
+        
+//         while(curr){
+            
+            
+//             if(curr -> val <x){
+//                 q1.push(curr);
+//             }else{
+//                 q2.push(curr);
+//             }
+//             curr = curr -> next;
+//         }
+        
+        
+        
+//         ListNode* newhead = NULL;
+//         if(!q1.empty()){
+//             newhead = q1.front();
+//             q1.pop();
+//         }else{
+//             newhead = q2.front();
+//             q2.pop();
+//         }
+        
+        
+//         curr = newhead;
+//         while(!q1.empty()){
+            
+//             curr -> next =  q1.front();
+//             q1.pop();
+//             curr = curr -> next;
+            
+            
+            
+            
+            
+//         }
+        
+//         while(!q2.empty()){
+            
+//             curr -> next = q2.front();
+//             q2.pop();
+//             curr = curr -> next;
+//         }
+//         curr -> next = NULL;
+        
+//         return newhead;
+        
+        
+//         two pointer
+        
+        ListNode* lesser = NULL;
+        ListNode* lesserhead = NULL;
+        
+        ListNode* greater = NULL;
+        ListNode* greaterhead = NULL;
+        
+        
+        ListNode* curr = head;
+        
         if(head == NULL)
-            return NULL;
+            return head;
         
         while(curr){
             
-            
             if(curr -> val <x){
-                q1.push(curr);
+                
+                if(lesserhead ==NULL){
+                    lesserhead = curr;
+                    lesser = curr;
+                    // lesser = lesser -> next;
+                }else{
+                    lesser -> next = curr;
+                    lesser = lesser -> next;
+                }
+                
+                
             }else{
-                q2.push(curr);
+                
+                if(greaterhead == NULL){
+                    greaterhead = curr;
+                    greater = curr;
+                }else{
+                    greater -> next = curr;
+                    greater = greater -> next;
+                }
+                
+                
+                
+                
             }
+            
             curr = curr -> next;
         }
+        if(greater)
+        greater -> next = NULL;
         
-        
-        
-        ListNode* newhead = NULL;
-        if(!q1.empty()){
-            newhead = q1.front();
-            q1.pop();
+        if(lesserhead){
+            lesser -> next = greaterhead;
+            return lesserhead;
         }else{
-            newhead = q2.front();
-            q2.pop();
+        
+            return greaterhead;
         }
         
         
-        curr = newhead;
-        while(!q1.empty()){
-            
-            curr -> next =  q1.front();
-            q1.pop();
-            curr = curr -> next;
-            
-            
-            
-            
-            
-        }
         
-        while(!q2.empty()){
-            
-            curr -> next = q2.front();
-            q2.pop();
-            curr = curr -> next;
-        }
-        curr -> next = NULL;
         
-        return newhead;
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
