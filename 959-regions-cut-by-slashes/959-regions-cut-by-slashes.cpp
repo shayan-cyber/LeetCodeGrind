64583060a7e5a1528ccec07aa813dafbbc60f3fa
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-14 19:45:13 +0530

    Time: 5 ms (91.61%), Space: 9.3 MB (98.51%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    vector <int> parent;
-    vector <int> rank;
+    int parent[31*31];
+    int rank[31*31];
     int count =1;
     void makeset(){
         
-        for(int i=0; i<parent.size(); i++){
+        for(int i=0; i<31*31; i++){
             parent[i] =i;
             rank[i] =0;
         }
@@ -49,10 +49,7 @@ public:
         
         
         int n = grid.size();
-       
         int dots = n+1;
-        parent = vector<int> (dots*dots+1);
-        rank = vector<int> ( dots*dots+1);
         makeset();
         for(int i=0; i<dots; i++){
             for(int j =0; j<dots; j++){
@@ -72,16 +69,15 @@ public:
         
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j] == '/'){
+                if(grid[i][j] == '\\'){
+                   int cell1 = i*dots + j;
+                   int cell2 = (i+1)*dots +j+1; 
+                    uni(cell1, cell2);
+                 }else if(grid[i][j] == '/'){
                     int cell1= (i+1)*dots + j;
                     int cell2 = i*dots + j+1;
                     uni(cell1,cell2);
                 }
-                else if(grid[i][j] == '\\'){
-                   int cell1 = i*dots + j;
-                   int cell2 = (i+1)*dots +j+1; 
-                    uni(cell1, cell2);
-                 } 
             }
             
             
