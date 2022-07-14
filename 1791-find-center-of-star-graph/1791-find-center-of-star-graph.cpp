70fd05ee1e47306d3732e56eaa35b9ec6093b8b7
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-14 20:15:53 +0530

    Time: 398 ms (23.53%), Space: 67.3 MB (53.11%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -27,37 +27,44 @@ public:
         
         
         
-//         using adj list
+//         using visited array
         
         
-        int n = edges.size();
-        int visited[n+2];
+//         int n = edges.size();
+//         int visited[n+2];
         
-        for(int i=0; i<n+2; i++){
-            visited[i] = 0;
-        }
+//         for(int i=0; i<n+2; i++){
+//             visited[i] = 0;
+//         }
         
         
-        for(int i=0; i<n; i++){
+//         for(int i=0; i<n; i++){
             
-            if(visited[edges[i][0]] ==0 ){
-                visited[edges[i][0]]++;
-            }else{
-                return edges[i][0];
-            }
+//             if(visited[edges[i][0]] ==0 ){
+//                 visited[edges[i][0]]++;
+//             }else{
+//                 return edges[i][0];
+//             }
             
             
             
-            if(visited[edges[i][1]] == 0){
-                visited[edges[i][1]]++;
+//             if(visited[edges[i][1]] == 0){
+//                 visited[edges[i][1]]++;
                 
-            }else{
-                return edges[i][1];
-            }
-        }
+//             }else{
+//                 return edges[i][1];
+//             }
+//         }
         
         
-        return -1;
+//         return -1;
+        
+        
+//         best
+        
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        return edges[0][1];
         
         
         
