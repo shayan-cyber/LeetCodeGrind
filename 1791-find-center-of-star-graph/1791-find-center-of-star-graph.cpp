ad320dfa1a4b8605ab9f387b650177194922d309
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-14 20:12:53 +0530

    Time: 345 ms (37.83%), Space: 67.7 MB (39.69%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -2,24 +2,65 @@ class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
         
-        unordered_map <int,int> mp;
+//         using hashmap(naive)
+//         unordered_map <int,int> mp;
         
         
-        for(int i=0; i<edges.size(); i++){
+//         for(int i=0; i<edges.size(); i++){
             
-            mp[edges[i][0]] ++;
-            mp[edges[i][1]] ++;
+//             mp[edges[i][0]] ++;
+//             mp[edges[i][1]] ++;
             
             
+//         }
+        
+        
+        
+//         for(auto it:mp){
+//             if(it.second== edges.size())
+//                 return it.first;
+//         }
+        
+//         return -1;
+        
+        
+        
+        
+        
+//         using adj list
+        
+        
+        int n = edges.size();
+        int visited[n+2];
+        
+        for(int i=0; i<n+2; i++){
+            visited[i] = 0;
         }
         
         
-        
-        for(auto it:mp){
-            if(it.second== edges.size())
-                return it.first;
+        for(int i=0; i<n; i++){
+            
+            if(visited[edges[i][0]] ==0 ){
+                visited[edges[i][0]]++;
+            }else{
+                return edges[i][0];
+            }
+            
+            
+            
+            if(visited[edges[i][1]] == 0){
+                visited[edges[i][1]]++;
+                
+            }else{
+                return edges[i][1];
+            }
         }
+        
         
         return -1;
+        
+        
+        
+        
     }
 };
