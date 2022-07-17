1edeb54db351b16fdcfded0895fc7b655c3c5bd8
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-07-17 12:38:31 +0530

    Time: 30 ms (63.10%), Space: 24 MB (92.72%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -11,45 +11,95 @@
  */
 class Solution {
 public:
+//     Naive approach
+    // unordered_map <int, int> mp;
     
-    unordered_map <int, int> mp;
-    void dfs(TreeNode* root){
+//     void dfs(TreeNode* root){
         
+//         if(root == NULL)
+//             return ;
+//         mp[root -> val] ++;
+//         dfs(root -> left);
+//         dfs(root -> right);
+        
+        
+        
+        
+//     }
+    
+    
+//     vector<int> findMode(TreeNode* root) {
+//         dfs(root);
+//         vector<int> res ;
+//         int freq =0 ;
+//         for(auto it: mp){
+//             if(it.second > freq){
+//                 freq = it.second;
+//             }
+              
+           
+//         }
+        
+//          for(auto it: mp){
+//             if(it.second == freq){
+//                 res.push_back(it.first);
+//             }
+              
+           
+//         }
+        
+            
+            
+        
+//         return res;
+//     }
+    
+    
+    
+//     best approach
+    
+    
+    int currfreq =0;
+    int prev = INT_MAX;
+    int mxfreq = INT_MIN;
+    vector<int> res;
+    
+    
+    void inorder(TreeNode* root){
         if(root == NULL)
-            return ;
-        mp[root -> val] ++;
-        dfs(root -> left);
-        dfs(root -> right);
+            return;
         
+        inorder(root -> left);
+        if(root -> val == prev){
+            currfreq++;
+        }else{
+            currfreq =1;
+        
+        }
+        
+        if(currfreq > mxfreq){
+            res.clear();
+            res.push_back(root-> val);
+            
+            mxfreq = currfreq;
+            
+        }else if(currfreq == mxfreq){
+            res.push_back(root -> val);
+        }
+        prev = root -> val;
+        inorder(root -> right);
         
         
         
     }
     
     
-    vector<int> findMode(TreeNode* root) {
-        dfs(root);
-        vector<int> res ;
-        int freq =0 ;
-        for(auto it: mp){
-            if(it.second > freq){
-                freq = it.second;
-            }
-              
-           
-        }
-        
-         for(auto it: mp){
-            if(it.second == freq){
-                res.push_back(it.first);
-            }
-              
-           
-        }
-        
-            
-            
-        
-        return res;
-    }
+     vector<int> findMode(TreeNode* root) {
+         
+         inorder(root);
+         return res;
+         
+         
+     }
+    
 };
