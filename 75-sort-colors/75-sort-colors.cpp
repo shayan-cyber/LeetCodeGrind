da400dffc6806f51fd252273de1ad7e9661d1c79
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-08-09 20:27:35 +0530

    Time: 4 ms (45.02%), Space: 8.3 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,32 +2,65 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        int count0=0;
-        int count1=0;
-        int count2=0;
+//         int count0=0;
+//         int count1=0;
+//         int count2=0;
         
-        int n = nums.size();
-        for(int i=0; i<n; i++){
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
             
-            if(nums[i] == 0)
-                count0++;
-            else if(nums[i] == 1)
-                count1++;
-            else
-                count2++;
+//             if(nums[i] == 0)
+//                 count0++;
+//             else if(nums[i] == 1)
+//                 count1++;
+//             else
+//                 count2++;
+//         }
+        
+        
+        
+//         for(int i=0; i<count0; i++){
+//             nums[i] = 0;
+//         }
+//         for(int i=count0; i<count0+ count1; i++){
+//             nums[i] =1;
+//         }
+        
+//         for(int i=count0 + count1; i<n; i++){
+//             nums[i] = 2;
+//         }
+        
+//         optimized 
+        int n = nums.size();
+        int low =0;
+        int mid =0;
+        int high = n-1;
+        
+        while(mid<= high){
+            
+            if(nums[mid] == 0){
+                
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+                
+                
+            }else if(nums[mid] == 2){
+                
+                swap(nums[mid], nums[high]);
+                high--;
+                
+            }else{
+                mid++;
+                
+            }
         }
         
         
         
-        for(int i=0; i<count0; i++){
-            nums[i] = 0;
-        }
-        for(int i=count0; i<count0+ count1; i++){
-            nums[i] =1;
-        }
         
-        for(int i=count0 + count1; i<n; i++){
-            nums[i] = 2;
-        }
+        
+        
+        
     }
 };
