bc1edde8d55a2d75404c89b7a708abcc9d8ba477
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-08-20 13:43:14 +0530

    Time: 405 ms (48.02%), Space: 116.7 MB (75.67%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -26,37 +26,79 @@ public:
 //             i++;
 //         }
         
-        int res = INT_MIN;
-        auto curr = head;
+//         second approach
+        
+//         int res = INT_MIN;
+//         auto curr = head;
 
-        int i=0;
-        queue <int> q;
-        stack <int> s;
+//         int i=0;
+//         queue <int> q;
+//         stack <int> s;
         
-        while(curr){
-            if(i< n/2){
-                q.push(curr -> val);
-            }else{
-                s.push(curr -> val);
-            }
+//         while(curr){
+//             if(i< n/2){
+//                 q.push(curr -> val);
+//             }else{
+//                 s.push(curr -> val);
+//             }
             
-            curr = curr -> next ;
-            i++;
-        }    
+//             curr = curr -> next ;
+//             i++;
+//         }    
         
         
         
         
-        while(!q.empty() && !s.empty()){
+//         while(!q.empty() && !s.empty()){
             
-            int t1 = q.front();
-            q.pop();
-            int t2 = s.top();
-            s.pop();
-            res = max(res, t1 + t2);
+//             int t1 = q.front();
+//             q.pop();
+//             int t2 = s.top();
+//             s.pop();
+//             res = max(res, t1 + t2);
             
+            
+//         }
+        
+//         third approach
+        auto slow = head;
+        auto fast = head;
+        int res = INT_MIN;
+        
+        while(fast && fast -> next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
             
         }
+        
+        
+        
+//         reverse
+       
+        ListNode* curr = slow;    
+        ListNode* nxt = slow -> next ;
+        ListNode* prev = NULL;
+         
+        while(nxt){
+            
+            curr -> next = prev;
+            prev = curr;
+            // nxt -> next = curr;
+            curr = nxt;
+            nxt = nxt -> next;
+   
+            
+        }
+        curr -> next = prev;
+        
+        while(curr && head){
+            res = max(res, curr -> val + head -> val);
+            curr = curr -> next;
+            head = head -> next;
+        }
+        
+        
+        
         
         
         
