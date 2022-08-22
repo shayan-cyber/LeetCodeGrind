a09504210c950a8ab90eff6f33c52b4c5a1c9178
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-08-22 20:11:21 +0530

    Time: 2 ms (50.72%), Space: 5.8 MB (89.85%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -5,14 +5,7 @@ public:
     vector<int> dp;
     
     int solve(int n){
-        if(n == 2){
-            return 1; 
-        }
-        if(n==3){
-            return 2;
-        }
-        
-        
+       
         if(n == 1)
             return 1;
         
@@ -21,13 +14,13 @@ public:
             return dp[n];
         
         
-        int temp =0;
+        int temp =n;
         
         
-        for(int j=1; j<n; j++){
+        for(int j=1; j<=n/2; j++){
             
             int temp2 = j*(n-j);
-            int temp3 =  j*solve(n-j);
+            int temp3 =  solve(j)*solve(n-j);
             int temp4 = max(temp2, temp3);
             temp = max(temp, temp4);
         }
@@ -47,6 +40,10 @@ public:
     int integerBreak(int n) {
         
         dp = vector<int> (60,-1);
+        if(n==2)
+            return 1;
+        if(n==3)
+            return 2;
       return   solve(n);
         
     }
