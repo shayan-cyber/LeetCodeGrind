04b5ca4e47797a1074106ee67b2f9934569fb7b5
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-08-25 18:27:57 +0530

    Time: 4 ms (65.00%), Space: 7.8 MB (24.83%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    
+    
     int solve(vector<int> & nums, int start , int end, bool turn){
         
         
        if(start > end ){
            return 0;
        }
-        
+        if(dp[start][end] != -1)
+            return dp[start][end];
         if(turn){
             
-             return max(solve(nums, start+1, end, false) + nums[start], solve(nums, start, end-1, false) + nums[end]);
+             return dp[start][end] = max(solve(nums, start+1, end, false) + nums[start], solve(nums, start, end-1, false) + nums[end]);
         
         }else{
-            return min(solve(nums, start+1, end, true), solve(nums, start, end-1, true));
+            return dp[start][end] = min(solve(nums, start+1, end, true), solve(nums, start, end-1, true));
         }
         
         
@@ -24,6 +28,9 @@ public:
     
     
     bool PredictTheWinner(vector<int>& nums) {
+        
+        dp = vector<vector<int>>(nums.size()+1, vector<int> (nums.size()+1, -1));
+        
         int sum =0;
         for(int i =0; i< nums.size(); i++){
             sum = sum + nums[i];
