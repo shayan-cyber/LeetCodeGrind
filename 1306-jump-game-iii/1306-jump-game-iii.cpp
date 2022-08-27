3615116615ef52def1f2e5736c46125b9ee127cb
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2022-08-27 12:02:17 +0530

    Time: 109 ms (13.73%), Space: 35.2 MB (42.19%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -2,21 +2,65 @@ class Solution {
 public:
     vector <bool> vis;
     
-    bool solve(vector<int> & arr, int start){
+//     bool solve(vector<int> & arr, int start){
         
-        if(start >= arr.size() || start < 0)
-            return false;
+//         if(start >= arr.size() || start < 0)
+//             return false;
         
-        if(vis[start] == true)
+//         if(vis[start] == true)
+//             return false;
+//         vis[start] =true;
+        
+        
+//         if(arr[start] == 0)
+//             return true;
+//         // if(dp[start] != -1)
+//         //     return dp[start];
+//         return  solve(arr, start - arr[start]) || solve(arr, start + arr[start]);
+        
+        
+        
+//     }
+    
+    
+    bool solve(vector<int>& arr, int start){
+        
+        if(start < 0 || start >= arr.size())
             return false;
-        vis[start] =true;
         
         
         if(arr[start] == 0)
             return true;
-        // if(dp[start] != -1)
-        //     return dp[start];
-        return  solve(arr, start - arr[start]) || solve(arr, start + arr[start]);
+        
+        queue <int> q;
+        q.push(start);
+        unordered_set <int> vis;
+        
+        while(!q.empty()){
+            
+            int top = q.front();
+            q.pop();
+            vis.insert(top);
+            if(arr[top] == 0)
+                return true;
+            
+            int first = top + arr[top];
+            int second = top - arr[top];
+            
+            if(first < arr.size() && vis.find(first) == vis.end()){
+                q.push(first);
+                
+                
+                
+            }
+            
+            if(second >=0 && vis.find(second) == vis.end()){
+                q.push(second);
+            }
+            
+        }
+        
+        return false;
         
         
         
